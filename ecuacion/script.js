ff4742bf2c6d2b639109bb7afaf3113cffbfd039
daniel <d.r.ferrer8@gmail.com>
2025-09-24 15:40:28 +0200

    eliminacion de funcion antigua

--- ecuacion/script.js
+++ ecuacion/script.js
@@ -2,13 +2,12 @@ for (let a = -5; a < 6; a++){
     for(let b = -3; b < 4; b++){
         for(let c = 0; c < 5; c++){
             console.log
-            (`a:${a} b:${b} c:${c} = ` +
-            ecuacion2(a, b, c)
+            (`a:${a} b:${b} c:${c} = ` + ecuacion(a, b, c)
             )
         }
     }
 }
-function ecuacion2(a, b, c){
+function ecuacion(a, b, c){
     const sqrt = Math.sqrt((Math.pow(b, 2)) + (-4 * a * c))
     const denominador = 2 * a
 
@@ -31,16 +30,3 @@ function ecuacion2(a, b, c){
     }
 }
 
-
-
-
-function ecuacion(a, b, c){
-    const raiz = Math.sqrt(Math.pow(b, 2) -4 * a * c)
-    const resultados = []
-    if(raiz >= 0){
-        resultados.push((-b + raiz) / (2 * a))
-        resultados.push((-b - raiz) / (2 * a))
-
-    }
-    return resultados
-}

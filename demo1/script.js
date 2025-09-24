040ce47f48f07e152f78863b11ff55cb48770b44
daniel <d.r.ferrer8@gmail.com>
2025-09-24 10:40:13 +0200

    variables js

---
+++ demo1/script.js
@@ -0,0 +1,37 @@
+/*
+* const x = 4
+* const y = 4.5
+* const z = "4"
+*
+* + - * / % ^
+*
+* < > >= <=
+* == === != !==
+*
+* x + y
+* x + y + z
+* x - y
+* x - y - z
+* x == z
+* x === z
+* x != z
+* x !== z
+* x < y
+* y < z
+* */
+
+const x = 4
+const y = 4.5
+const z = "4"
+
+
+console.log(x + y)
+console.log(x + y + z)
+console.log(x - y)
+console.log(x - y - z)
+console.log(x == z)
+console.log(x === z)
+console.log(x != z)
+console.log(x !== z)
+console.log(x < y)
+console.log(y < z)
